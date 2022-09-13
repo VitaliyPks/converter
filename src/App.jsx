ae02ed5447faf8dcd9ecdfb7b82810274e774927
Vitaliy <vetal9305@gmail.com>
2022-09-13 03:10:09 +0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -10,72 +10,83 @@ function App() {
   const [value2, setValue2] = useState(null);
 
   async function getData() {
-    const response = await fetch('https://api.apilayer.com/currency_data/live', {
-      headers: { apikey: 'nFI1a1wvEczOfqn22zkN8rpN6lrkQLfK' },
+    const response = await fetch('https://api.apilayer.com/currency_data/list', {
+      headers: { apikey: 'ykhf0AURgo7QYodMitcoxPqxI2ja9U7w' },
     });
     const data = await response.json();
-    setCurrency1(data.quotes);
-    setCurrency2(data.quotes);
+
+    setCurrency1(data.currencies);
+    setCurrency2(data.currencies);
   }
 
   useEffect(() => {
     getData();
   }, []);
 
-  function converter(e) {
+  async function converter(e) {
     e.preventDefault();
-    const convertCurrency = (value1 / value2) * input1;
-    setInput2(convertCurrency);
+    const response = await fetch(`https://api.apilayer.com/currency_data/live?source=${value1}&currencies=${value2}`, {
+      headers: { apikey: 'ykhf0AURgo7QYodMitcoxPqxI2ja9U7w' },
+    });
+    const data = await response.json();
+    const course = data.quotes[`${value1}${value2}`];
+    const num = (course * (input1 || 1)).toFixed(2);
+    setInput2(num);
   }
 
   return (
-    <div className="container">
-      <h3>Converter</h3>
-      <form onSubmit={converter}>
-        <div>
-          <input
-            type="text"
-            value={input1 || ''}
-            onChange={(e) => setInput1(e.target.value)} />
-          <select
-            onChange={(e) => setValue1(e.target.value)}>
-            {currency1
-              ? (Object.keys(currency1)
-                .map((value, index) => (<option
-                  key={index}
-                  value={currency1[value]}>
-                  {value.slice(3, value.length)}
-                </option>)))
-              : (<option>AED</option>)
-            }
-          </select>
-        </div>
-        <div>
-          <input
-            type="text"
-            value={input2 || ''}
-            readOnly />
-          <select
-            onChange={(e) => setValue2(e.target.value)}>
-            {currency2
-              ? (Object.keys(currency2)
-                .map((value, index) => (<option
-                  key={index}
-                  value={currency1[value]}>
-                  {value.slice(3, value.length)}
-                </option>)))
-              : (<option>AED</option>)
-            } {currency1
-              ? (<option value={1} key={999}>
-                {Object.keys(currency1)[0].slice(0, 3)}
-              </option>)
-              : (<></>)
-            }
-          </select>
-        </div>
-        <button type='submit'>convert</button>
-      </form>
-    </div>
+    <>
+      <div className="container">
+        {currency1 ? (
+          <>
+            <h3>Converter</h3>
+            <form onSubmit={converter}>
+              <div>
+                <input
+                  type="text"
+                  value={input1 || ''}
+                  onChange={(e) => setInput1(e.target.value)} />
+                <select
+                  onChange={(e) => setValue1(e.target.value)}>
+                  {currency1
+                    ? (Object.keys(currency1)
+                      .map((value, index) => (<option
+                        key={index}
+                        value={value}>
+                        {value}
+                      </option>)))
+                    : (<option>AED</option>)
+                  }
+                </select>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  value={input2 || ''}
+                  readOnly />
+                <select
+                  onChange={(e) => setValue2(e.target.value)}>
+                  {currency2
+                    ? (Object.keys(currency2)
+                      .map((value, index) => (<option
+                        key={index}
+                        value={value}>
+                        {value}
+                      </option>)))
+                    : (<option>AED</option>)
+                  }
+                </select>
+              </div>
+              <button type='submit'>convert</button>
+            </form>
+          </>
+        ) : (
+          <div className='circle'>
+            <img src='./2492300.png' alt="..." />
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
